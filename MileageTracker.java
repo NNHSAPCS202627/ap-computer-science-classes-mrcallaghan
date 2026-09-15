8a8bfc3708189959a92b7ac4c818f3ea869bc681
Matt Callaghan <mcallaghan@naperville203.org>
2026-09-15 14:19:25 -0500

    Wrote notes on instance variables and constructors

--- MileageTracker.java
+++ MileageTracker.java
@@ -8,23 +8,63 @@ public class MileageTracker
 {
     /*
      * 2. Define the instance variables (to store the object's attributes):
+     *      - specify the visibility (e.g., private)
+     *          - public: accessible by any code in any class
+     *          - private: only accessible by methods in this class
+     *      - specify the type (e.g., int, String, Turtle, etc.)
+     *      - specify the name (e.g., distanceDriven)
+     *      
+     *      Instance variables differ from local variables in teh following ways:
+     *          - scoped to the class (accessible in all methods in the class)
+     *          - lifetime is the same as that of the object
+     *          - automatically initialized to a default value (0, false, null)
+     *          - best practice is not to immediately initialize instance variables (but do so in the constructor)
      */
+    private int distanceDriven;         // in units of miles
+    private int fuelConsumed;           // in unit of gallons
+    private String vin;                 // vehicle identification number
 
 
-
-
-
-
-    
     /*
      * 3. Define the constructor(s) to initialize objects:
+     *      - responsible for initializing newly created objects
+     *      - invoked automatically via the new operator
+     *      - name of the constructor must match the class name exactly
+     *      - has no return type (not even void)
+     *      - multiple constructors may be defined for a class
+     *      - one constructor can call another (with limitations)
      */
+    /**
+     * Default constructor for the MileageTracker class.
+     *      Initializes the object with values of 0 miles driven, 0 fuelConsumed, and a null vin.
+     */
+    public MileageTracker()
+    {
+        /*
+         * The "this" reserved word references the current object (like "self" in Python).
+         *      It's use in highly encouraged, but not require in most cases.
+         */
+        this.distanceDriven = 0;
+        this.fuelConsumed = 0;
+        this.vin = null;
+        
+    }
+    
+    /**
+     * Constructor that initializes a new MileageTrackr object with a specified miles driven and fuel consumed.
+     * 
+     * @param initalDistanceDriven the number of miles alread driven
+     * @param initialFuelConsumed the number of gallons of fuel already consumed
+     */
+    public MileageTracker(int initialDistanceDriven, int initialFuelConsumed)
+    {
+        this.vin = null;
+    }
     
     
     
     
-    
-    
+     
     
     
     /*
@@ -44,7 +84,7 @@ public class MileageTracker
      */
     public void incrementDistanceDriven(int miles)
     {
-        
+        // to be implemented later
     }
     
     /**
@@ -114,8 +154,13 @@ public class MileageTracker
     {
         //vin = vin;
     }
+    
+    
+    public static void main()
+    {
+        // client code goes here
+    }
 }
-
 
 
 
