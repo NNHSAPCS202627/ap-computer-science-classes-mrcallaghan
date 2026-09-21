c0e6a5ce5c4e981d5b501a705346a89e2e4db4be
Matt Callaghan <mcallaghan@naperville203.org>
2026-09-21 08:03:39 -0500

    Wrote warmups 1-3

--- WaterBottle.java
+++ WaterBottle.java
@@ -2,5 +2,23 @@ import java.awt.Color;
 
 public class WaterBottle
 {
+    // Warm-up #1: instance variables
+    private Color color;
+    private double fluidAmount;
 
+    // Warm-up #2: default constructor
+    public WaterBottle()
+    {
+        this.color = Color.RED;
+        this.fluidAmount = 0.0;
+    }
+
+    // Warm-up #3: custom constructor
+    public WaterBottle(Color initialColor, double initialFluidAmount)
+    {
+        this.color = initialColor;
+        this.fluidAmount = initialFluidAmount;
+    }
+
+    // List of methods
 }
